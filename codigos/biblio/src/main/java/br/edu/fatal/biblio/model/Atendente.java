afaa9f61f539d626b7b8c834d9dded31fb8b45ef
Marcelo <mnr@cin.ufpe.br>
2017-05-24 11:21:52 -0300

    nova estruturacao

---
+++ codigos/biblio/src/main/java/br/edu/fatal/biblio/model/Atendente.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatal.biblio.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ffmas
+ */
+@Entity
+@Getter
+@Setter
+public class Atendente extends Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(mappedBy = "atendente")
+    private Set<Emprestimo> emprestimosIntermediados = new HashSet();
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Atendente)) {
+            return false;
+        }
+        Atendente other = (Atendente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.edu.fatal.biblio.model.Atendente[ id=" + id + " ]";
+    }
+    
+}
